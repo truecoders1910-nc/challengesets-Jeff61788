a7bbe6898d575726ac679e90a0638396b94831c6
jeff <jeffaycock3@gmail.com>
2019-12-18 13:44:08 -0600

    4 of 10 completed

--- ChallengeSets/ChallengeSet04.cs
+++ ChallengeSets/ChallengeSet04.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Linq;
+
 namespace ChallengeSets
 {
     public class ChallengeSet04
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    result += num;
+                }
+                if (num % 2 != 0)
+                {
+                    result -= num;
+                }
+            }
+            return result;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
@@ -15,7 +29,8 @@ namespace ChallengeSets
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] array = { number1, number2, number3, number4 };
+            return array.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
@@ -30,22 +45,67 @@ namespace ChallengeSets
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            int myInt;
+            decimal value;
+            if (int.TryParse(input, out myInt))
+            {
+                return true;
+            }
+            if (Decimal.TryParse(input, out value))
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
-        }
+            object majority = null;
+            int count = 0;
+            object[] array = { };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(majority))
+                {
+                    count++;
+                }
+            }
+            if (count > (array.Length / 2))
+            {
+                return true;
+            }
+            return false;
 
+        }
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            double result = 0;
+            double average = numbers.Average();
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    result += number;
+                }
+            }
+            return average;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
         }
     }
 }
